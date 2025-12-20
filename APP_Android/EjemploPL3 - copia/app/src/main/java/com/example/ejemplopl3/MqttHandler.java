32fc7b70b6d4fe1ca0fa8a28818851dd1ee213b0
oski-214 <oscar.moran@edu.uah.es>
2025-12-20 12:25:25 +0100

    quitar  comentarios

--- APP_Android/EjemploPL3 - copia/app/src/main/java/com/example/ejemplopl3/MqttHandler.java
+++ APP_Android/EjemploPL3 - copia/app/src/main/java/com/example/ejemplopl3/MqttHandler.java
@@ -3,7 +3,7 @@ package com.example.ejemplopl3;
 import android.util.Log;
 import com.hivemq.client.mqtt.MqttClient;
 import com.hivemq.client.mqtt.MqttGlobalPublishFilter;
-import com.hivemq.client.mqtt.datatypes.MqttQos; // Importar MqttQos
+import com.hivemq.client.mqtt.datatypes.MqttQos;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5AsyncClient;
 import com.hivemq.client.mqtt.mqtt5.message.publish.Mqtt5Publish;
 import java.util.UUID;
@@ -41,7 +41,6 @@ public class MqttHandler {
         }
         client.subscribeWith()
                 .topicFilter(topic)
-                // CORRECCIÓN APLICADA AQUÍ:
                 .qos(MqttQos.fromCode(qos))
                 .send()
                 .whenComplete((subAck, throwable) -> {
